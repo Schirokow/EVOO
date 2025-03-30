0bbda443214fddb6c44c94d31d1d7fc1b8b72e44
Alexander Hansen <hansenjanssen@gmail.com>
2025-03-30 20:55:06 +0200

    test commit

--- app/src/main/java/com/example/evoo/ui/theme/Color.kt
+++ app/src/main/java/com/example/evoo/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundColor = Color(0xFF20587B)
+val ForegroundColor = Color(0xFFED6E63)
+val AccentColor = Color(0xFF29719E)
+
+
+val BottomDarkBlue = Color(0xFF1A4D6C)
+val TopLightBlue = Color(0xFF62A7C3)

3cb378f5de9c1374da64f4d3a1b3aabc6810f812
Schirokow <165018073+Schirokow@users.noreply.github.com>
2025-04-12 16:20:42 +0200

    ContentDetailScreen -> Bildgröße und Textgröße angepasst, Button mit LocationScreen verknüpft.

--- app/src/main/java/com/example/evoo/ui/screens/ContentDetailScreen.kt
+++ app/src/main/java/com/example/evoo/ui/screens/ContentDetailScreen.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
@@ -24,11 +27,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.evoo.AccentColor
 import com.example.evoo.BottomDarkBlue
@@ -89,7 +94,8 @@ fun ContentDetailScreen(navController: NavController, index: Int){
                 Text(
                     text = festivalData.title,
                     style = MaterialTheme.typography.headlineLarge,
-                    color = Color.White
+                    color = Color.White,
+                    modifier = Modifier.padding(bottom = 8.dp)
                 )
 
                 // Bild
@@ -98,15 +104,16 @@ fun ContentDetailScreen(navController: NavController, index: Int){
                     contentDescription = null,
                     modifier = Modifier
                         .fillMaxWidth(0.9f)
+                        .fillMaxHeight(0.5f)
                         //.padding(16.dp)
                         .clip(RoundedCornerShape(12.dp)),
-                    contentScale = ContentScale.Crop
+                    contentScale = ContentScale.FillBounds
                 )
 
                 // Beschreibung
                 Text(
                     text = "Beschreibung: ${festivalData.description}",
-                    style = MaterialTheme.typography.bodyLarge,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontSize = 25.sp),
                     color = Color.White,
                     modifier = Modifier.padding(16.dp)
                 )
@@ -114,7 +121,7 @@ fun ContentDetailScreen(navController: NavController, index: Int){
                 // Datum
                 Text(
                     text = "Datum: ${festivalData.datum}",
-                    style = MaterialTheme.typography.bodyLarge,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontSize = 25.sp),
                     color = Color.White,
                     modifier = Modifier.padding(16.dp)
                 )
@@ -122,13 +129,14 @@ fun ContentDetailScreen(navController: NavController, index: Int){
                 // Location
                 Text(
                     text = "Veranstaltungsort: ${festivalData.location}",
-                    style = MaterialTheme.typography.bodyLarge,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontSize = 25.sp),
                     color = Color.White,
                     modifier = Modifier.padding(16.dp)
                 )
 
+                Spacer(modifier = Modifier.height(20.dp))
                 Button(
-                    onClick = {}
+                    onClick = {navController.navigate("LocationScreen")}
                 ) {
                     Text(text = "Auf der Karte zeigen")
                 }

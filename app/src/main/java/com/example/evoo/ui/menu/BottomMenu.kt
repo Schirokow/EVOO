0d89d9a058cef13edc25388009dbcaec97f54b23
Alexander Hansen <hansenjanssen@gmail.com>
2025-04-28 21:36:55 +0200

    erste version von BottomMenu erstellt(in Bearbeitung)

--- app/src/main/java/com/example/evoo/ui/menu/BottomMenu.kt
+++ app/src/main/java/com/example/evoo/ui/menu/BottomMenu.kt
@@ -1,26 +1,27 @@
 package com.example.evoo.ui.menu
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Box
+import com.example.evoo.R
+import com.example.evoo.ui.theme.colorthemetype.BottomDarkBlue
+import androidx.compose.foundation.Image
+import androidx.compose.ui.draw.shadow
 
 @Preview
 @Composable
@@ -28,6 +29,7 @@ fun PreviewAnyeBottomBar() {
     AnyeBottomBar(
         onHomeClick = {},
         onSearchClick = {},
+        onProfileClick = {},
         onSettingsClick = {},
         onAnyeClick = {}
     )
@@ -40,28 +42,73 @@ fun PreviewAnyeBottomBar() {
 fun AnyeBottomBar(
     onHomeClick: () -> Unit,
     onSearchClick: () -> Unit,
+    onProfileClick: () -> Unit,
     onSettingsClick: () -> Unit,
     onAnyeClick: () -> Unit
 ) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.Gray.copy(alpha = 0.1f))
     ) {
         BottomAppBar(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(16.dp)
-    ) {
+                .padding(16.dp) ,
+            containerColor = BottomDarkBlue
+        ) {
+
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceBetween
-            ){
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 IconButton(onClick = onHomeClick) {
                     Icon(
-                        imageVector = Icons.Filled.Home
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = "Home",
+                        tint = Color.White
+                    )
+                }
+                IconButton(onClick = onSearchClick) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search",
+                        tint = Color.White
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(56.dp))
+
+                IconButton(onClick = onProfileClick) {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "Profile",
+                        tint = Color.White
+                    )
+                }
+                IconButton(onClick = onSettingsClick) {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "Settings",
+                        tint = Color.White
                     )
                 }
             }
         }
-}}
+        Box(
+            modifier = Modifier
+                .size(96.dp)
+                .align(Alignment.BottomCenter)
+                .offset(y = (-25).dp)
+                .background(BottomDarkBlue, CircleShape)
+                .clickable {onAnyeClick()}
+                .shadow(4.dp, CircleShape ),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_anye),
+                contentDescription = "AnyE Logo",
+                modifier = Modifier.size(58.dp)
+            )
+        }
+    }}
